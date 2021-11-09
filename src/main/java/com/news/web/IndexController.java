f86563e471197c957a1384c7f13afd70346020d4
gwnnho <rhksgh99@gmail.com>
2021-11-09 23:49:34 +0900

    spring.main.allow-bean-definition-overriding=true 추가

--- src/main/java/com/news/web/IndexController.java
+++ src/main/java/com/news/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.news.web;
 
+import com.news.config.auth.LoginUser;
+import com.news.config.auth.dto.SessionUser;
 import com.news.service.PostsService;
 import com.news.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
         return  "index";
     }
 
